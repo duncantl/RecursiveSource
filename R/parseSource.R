f54a1d47105f27f646a25d6700d3960c62669579
Duncan Temple Lang <duncan@r-project.org>
2022-03-05 14:15:15 -0800

    psource that shows the approach of finding calls to source and modifying them and then passing the expressions to source().  Also have to handle chdir in the function, not leave it to source() as we are passing expressions and cannot also pass the file parameter needed for chdir in source

---
+++ R/parseSource.R
@@ -0,0 +1,54 @@
+#
+psource =
+function()
+{
+    k = match.call()
+       # need to leave the file for chdir to be able to take effect. but can't have file and exprs
+    k = k[-2] 
+    exprs = as.list(parse(file))
+    exprs = updateCallsToSource(exprs, k)
+
+    k$exprs = exprs
+    k[[1]] = as.name("source")
+    print(getwd())
+    if(chdir) {
+        cwd = getwd()
+        on.exit(setwd(cwd))
+        setwd(dirname(file))
+    }    
+    eval(k, parent.frame())
+}
+
+formals(psource) = formals(source)
+
+updateCallsToSource =
+function(exprs, origCall)
+{
+
+    # Make this more general to find the source() calls anywhere, including nested in if(), etc.
+    #    idx = findCallsTo(exprs, "source", index = TRUE)
+    # For now, look at top-level calls.
+    idx = which(sapply(exprs, isCallTo, "source"))
+
+    if(length(idx)) {
+        # change to psource and add arguments from origCall.
+        exprs[idx] = lapply(exprs[idx], updateSourceCall, origCall)
+    }
+    exprs
+}
+
+updateSourceCall =
+function(call, origCall)    
+{
+    call = match.call(source, call)
+    origParams = names(origCall)[-1]
+    addParams = setdiff(origParams, names(call))
+    call[ addParams ] = origCall[addParams]
+    call[[1]] = as.name("psource")
+    call
+}
+
+
+
+
+
